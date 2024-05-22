ca2d012c94944e439703f11b742db62f34c26763
dspix <d.m.simms@cranfield.ac.uk>
2024-05-22 10:30:55 +0100

    Added code for greenup

---
+++ source/pheno.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+def get_greenup(x_time, period=[0, 366]):
+  """Returns index of greenup phenological date from 
+  multiyear time-series profile.
+
+  The greenup (or *start of season*)
+  defined as the start of the *land surface phenology*
+  profile. The function returns the index of the maximum
+  peak in curvature for a specific period for each year 
+  (e.g. spring), using the day-of-the-year to set the 
+  thresholds.
+
+  Arguments:
+    x_time - a pandas series with datetime index
+    period - day-of-year period for search
+  """
+  x = pd.DataFrame(x_time.copy(), columns=['value'])
+  x.sort_index(inplace=True)
+  x['year'] = x.index.year
+  
+  grad = np.gradient(x['value'].values)
+  x['curve'] = np.gradient(grad)*(1 + grad**2)**-1.5
+
+  t, f = period
+  if t<f:
+    sub = x[(x.index.dayofyear>t) & (x.index.dayofyear<f)]
+  else:
+    sub = x[(x.index.dayofyear>t) | (x.index.dayofyear<f)]
+
+  return sub.groupby('year')['curve'].idxmax().values
